acda360d782b8477879b0565a6c0799743368c7e
Deepak Yadav <youremail@example.com>
2026-05-22 15:36:43 +0530

    Added cloudinary dependencies

--- myproject/settings.py
+++ myproject/settings.py
@@ -149,3 +149,11 @@ INSTALLED_APPS = [
 ]
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+import os
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
